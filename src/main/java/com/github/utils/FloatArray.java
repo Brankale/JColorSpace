9792c47c7d4a80882473818a761706abf6cee7cf
Brankale <45881310+Brankale@users.noreply.github.com>
2022-04-09 15:41:46 +0200

    added FloatArray

---
+++ src/main/java/com/github/utils/FloatArray.java
@@ -0,0 +1,45 @@
+package com.github.utils;
+
+public class FloatArray {
+
+    private final float[] array;
+
+    /**
+     * Create a float array of the specified size
+     *
+     * @param size of teh array
+     */
+    public FloatArray(int size) {
+        array = new float[size];
+    }
+
+    /**
+     * @return the size of the array
+     */
+    public int size() {
+        return array.length;
+    }
+
+    /**
+     * Set a value at the specified index
+     *
+     * @param index of the array
+     * @param value to set
+     * @throws IndexOutOfBoundsException if the index is greater than the size
+     */
+    public void set(int index, float value) {
+        array[index] = value;
+    }
+
+    /**
+     * Get the value at the specified index
+     *
+     * @param index of the array
+     * @return the value stored at the index
+     * @throws IndexOutOfBoundsException if the index is greater than the size
+     */
+    public double get(int index) {
+        return array[index];
+    }
+
+}
