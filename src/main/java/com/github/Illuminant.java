144fe7318c02567ecc6adda5a15ba7efe250ab20
Brankale <45881310+Brankale@users.noreply.github.com>
2022-04-10 10:32:30 +0200

    added Illuminant class

---
+++ src/main/java/com/github/Illuminant.java
@@ -0,0 +1,10 @@
+package com.github;
+
+public class Illuminant {
+
+    private Illuminant() {
+        // hide constructor
+    }
+
+    public static final WhitePoint D65 = new WhitePoint(0.3127f, 0.3290f);
+}
