83ee934c760e7defa78eb15141ace1cb3a16a418
Brankale <45881310+Brankale@users.noreply.github.com>
2022-07-08 19:24:10 +0200

    use java conventions for constants names

--- src/main/java/com/github/colorspaces/ColorSpaces.java
+++ src/main/java/com/github/colorspaces/ColorSpaces.java
@@ -1,8 +1,6 @@
 package com.github.colorspaces;
 
 import com.github.ColorSpace;
-import com.github.Illuminant;
-import com.github.Primaries;
 import com.github.Rgb;
 
 public class ColorSpaces {
@@ -10,7 +8,8 @@ public class ColorSpaces {
         // hide constructor
     }
 
-    public static final ColorSpace CieLab = new CieLab();
-    public static final Rgb sRGB = new Srgb();
+    public static final ColorSpace CIE_LAB = new CieLab();
+    public static final Rgb SRGB = new Srgb();
+    public static final Rgb DCI_P3 = new DciP3();
 
 }
