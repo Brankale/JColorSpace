cb5797ae0c420b53d549ecfa45677ac2efa8b733
Brankale <45881310+Brankale@users.noreply.github.com>
2022-07-08 18:48:15 +0200

    you should not be able to create different versions of sRGB

--- src/main/java/com/github/colorspaces/ColorSpaces.java
+++ src/main/java/com/github/colorspaces/ColorSpaces.java
@@ -4,7 +4,6 @@ import com.github.ColorSpace;
 import com.github.Illuminant;
 import com.github.Primaries;
 import com.github.Rgb;
-import com.github.utils.FloatArray;
 
 public class ColorSpaces {
     private ColorSpaces() {
@@ -12,22 +11,6 @@ public class ColorSpaces {
     }
 
     public static final ColorSpace CieLab = new CieLab();
-    public static final Rgb sRgb = new Srgb(
-            "sRGB IEC61966-2.1",
-            Primaries.BT709,
-            Illuminant.D65,
-            d -> {
-                if (d < 0.0031308)
-                    return 12.92 * d;
-                else
-                    return 1.055 * Math.pow(d, 1 / 2.4) - 0.055;
-            },
-            d -> {
-                if (d < 0.04045)
-                    return d / 12.92;
-                else
-                    return Math.pow(((d + 0.055) / 1.055), 2.4);
-            }
-    );
+    public static final Rgb sRGB = new Srgb();
 
 }
--- src/main/java/com/github/colorspaces/Srgb.java
+++ src/main/java/com/github/colorspaces/Srgb.java
@@ -1,19 +1,27 @@
 package com.github.colorspaces;
 
+import com.github.Illuminant;
+import com.github.Primaries;
 import com.github.Rgb;
-import com.github.WhitePoint;
-import com.github.utils.FloatArray;
-
-import java.util.function.DoubleUnaryOperator;
 
 public class Srgb extends Rgb {
-    public Srgb(
-            String name,
-            FloatArray primaries,
-            WhitePoint whitePoint,
-            DoubleUnaryOperator oetf,
-            DoubleUnaryOperator eotf
-    ) {
-        super(name, primaries, whitePoint, oetf, eotf);
+    public Srgb() {
+        super(
+                "sRGB IEC61966-2.1",
+                Primaries.BT709,
+                Illuminant.D65,
+                d -> {
+                    if (d < 0.0031308)
+                        return 12.92 * d;
+                    else
+                        return 1.055 * Math.pow(d, 1 / 2.4) - 0.055;
+                },
+                d -> {
+                    if (d < 0.04045)
+                        return d / 12.92;
+                    else
+                        return Math.pow(((d + 0.055) / 1.055), 2.4);
+                }
+        );
     }
 }
