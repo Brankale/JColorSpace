2fe53d578a0321bd1558954d5d6ddd01b925ff47
Brankale <45881310+Brankale@users.noreply.github.com>
2022-07-30 23:10:16 +0200

    stylish things

--- src/main/java/com/github/brankale/jcolorspace/colorspaces/DciP3.java
+++ src/main/java/com/github/brankale/jcolorspace/colorspaces/DciP3.java
@@ -10,7 +10,7 @@ class DciP3 extends Rgb {
                 "SMPTE RP 431-2-2007 DCI (P3)",
                 new FloatArray(0.680f, 0.320f, 0.265f, 0.690f, 0.150f, 0.060f),
                 new WhitePoint(0.314f, 0.351f),
-                d -> Math.pow(d, 1 / 2.6),
+                d -> Math.pow(d, 1.0 / 2.6),
                 d -> Math.pow(d, 2.6)
         );
     }
