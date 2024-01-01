076810e137cdeb77f9ca3f2caaa369c1b21fcaf2
brankale <45881310+Brankale@users.noreply.github.com>
2024-01-01 21:23:05 +0100

    feature/kotlin-migration: kotlin migration #3

--- src/main/kotlin/com/github/brankale/jcolorspace/colorspace/rgb/WhitePoint.kt
+++ src/main/kotlin/com/github/brankale/jcolorspace/colorspace/rgb/WhitePoint.kt
@@ -1,15 +1,14 @@
-package com.github.brankale.jcolorspace.colorspace.rgb;
+package com.github.brankale.jcolorspace.colorspace.rgb
 
 /**
  * Class for constructing white points used in RGB color space.
  * The value is stored in the CIE XYZ color space.
  * Y component of white point is assumed to be 1.
  */
-public class WhitePoint {
-
-    public final float x;
-    public final float y;
-    public final float z;
+class WhitePoint {
+    @JvmField val x: Float
+    @JvmField val y: Float
+    @JvmField val z: Float
 
     /**
      * Construct WhitePoint with CIE xyY coordinates.
@@ -18,13 +17,13 @@ public class WhitePoint {
      * @param x The x coordinate of CIExyY
      * @param y The y coordinate of CIExyY
      */
-    public WhitePoint(float x, float y) {
+    constructor(x: Float, y: Float) {
         // X = (x * Y) / y
-        this.x = x / y;
+        this.x = x / y
         // Y is assumed to be 1
-        this.y = 1.0f;
+        this.y = 1.0f
         // Z = (z * Y) / y, where z = 1 - x - y
-        this.z = (1 - x - y) / y;
+        this.z = (1 - x - y) / y
     }
 
     /**
@@ -36,13 +35,11 @@ public class WhitePoint {
      *
      * @throws IllegalArgumentException if Y is different from 1.
      */
-    public WhitePoint(float x, float y, float z) {
-        if (y != 1.0f)
-            throw new IllegalArgumentException("White point cannot have Y != 1.");
+    constructor(x: Float, y: Float, z: Float) {
+        require(y == 1.0f) { "White point cannot have Y != 1." }
 
-        this.x = x;
-        this.y = 1.0f;
-        this.z = z;
+        this.x = x
+        this.y = 1.0f
+        this.z = z
     }
-
 }
